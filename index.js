fe921dd89352a2fa6b7d58ea6c983ab6f124e081
freddex <fredjuiceman@yahoo.com>
2022-05-17 18:08:26 -0400

    commit tree 936978d652092b4afe2d4fc4dd15fe79a4bb82a8

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (a, b) => a / b
+
+const square = x => x * x
+
+const add = (x, y) => x + y
+
+
+
+
+
+
+// function divide() {
+//   let answer = 2000 / 100
+//   return(answer)
+// }
+// let square = x => x * x;
+// let add = (a, b) => a + b;
